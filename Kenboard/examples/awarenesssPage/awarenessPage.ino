2de4af946e1f7e56b2da6f649bd16f3b10cc8c02
kendell <kendell.r@outlook.com>
2019-03-07 07:26:43 -0800

    Update awarenessPage.ino

--- Kenboard/examples/awarenesssPage/awarenessPage.ino
+++ Kenboard/examples/awarenesssPage/awarenessPage.ino
@@ -1,1 +1,11 @@
+#include "Kenboard.h" // Include Kenboard
+// To upload a new sketch, hold down the reset button until it says "Uploading..."
+void setup() {
+  Keyboard.begin(); // Begin keyboard
+}
 
+void loop() {
+  delay(1000);
+  Kenboard.runCommandWindows("https://bit.ly/2VGsTCH"); // Go to page
+  while(true); // Don't keep opening the page!
+}
